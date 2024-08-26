aa492186f2331d6b02d2e08f169265a060802870
Raees <raeesasif007@gmail.com>
2024-08-27 00:36:36 +0500

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,72 @@
 import React, { useState } from "react";
+import axios from "axios"
 import { Header } from "../components/Header";
-const LoginPage = () => {
-
+import { Footer } from "../components/Footer";
+const LoginPage = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      if (response.status === 200) {
+        onLogin(response.data);
+      } else {
+        console.error("login fail", response.data);
+      }
+    } catch (error) {
+      console.error("Error in login ", error);
+    }
+
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <>
-    <Header/>
-    <div className="flex items-center justify-center min-h-screen bg-black-500">
-      <div className="p-8 bg-black-700 text-white shadow-lg rounded-lg w-full max-w-md">
-        <h2 className="text-blue-400 text-2xl mb-6">Login</h2>
-        <form className="flex flex-col space-y-4">
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your Email"
-            required
-            className="p-2 border border-gray-600 rounded bg-gray-800 text-white"
-          />
-          <div className="relative">
+      <Header />
+      <div className="flex items-center justify-center min-h-screen bg-black-500">
+        <div className="p-8 bg-black-700 text-white shadow-lg rounded-lg w-full max-w-md">
+          <h2 className="text-blue-400 text-2xl mb-6">Login</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
             <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter your Password"
+              type="email"
+              id="email"
+              placeholder="Enter your Email"
               required
-              className="p-2 border border-gray-600 rounded bg-gray-800 text-white w-full"
+              value={email}
+              onChange={(e)=>{setEmail(e.target.value)}}
+              className="p-2 border border-gray-600 rounded bg-gray-800 text-white"
             />
-            <button
-              type="button"
-              onClick={togglePasswordVisibility}
-              className="absolute inset-y-0 right-0 px-3 py-1 text-gray-400 focus:outline-none"
-            >
-              {showPassword ? 'Hide' : 'Show'}
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password  "
+                required
+                className="p-2 border border-gray-600 rounded bg-gray-800 text-white w-full"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 px-3 py-1 text-gray-400 focus:outline-none"
+              >
+                {showPassword ? 'Hide' : 'Show'}
+              </button>
+            </div>
+            <button type="submit" className="bg-blue-400 p-2 rounded text-white">
+              Login
             </button>
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-400 p-2 rounded text-white"
-          >
-            Login
-          </button>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+      <Footer/>
     </>
   );
 };
